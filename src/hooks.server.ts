98cf4d21c5e8c29733ae78c1ceec6001ba0de681
hhs <vectra@126.com>
2026-08-24 17:12:57 +0800

    改为session鉴权，简化api调用核心函数

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,7 +2,5 @@ import { initDb } from "$lib/models/db";
 import type { ServerInit } from "@sveltejs/kit";
 
 export const init: ServerInit = async () => {
-    console.log("init");
-    
     await initDb();
 };
